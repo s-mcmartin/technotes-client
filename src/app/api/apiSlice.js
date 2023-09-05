8311eb981e5cda4741f36b97e56db6ce9f6fadf7
s-mcmartin <shannoncmcguire@outlook.com>
2023-09-05 18:11:12 -0400

    change baseURL

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://technotes-w3xe.onrender.com/",
+  baseUrl: "https://technotes-api-vqsf.onrender.com/",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
